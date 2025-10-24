aac58e1dac580203ce81eaf2b216c25c3c7db9ba
TusharJain86 <tusharjain0806@gmail.com>
2025-10-25 00:12:10 +0530

    initail start of project

---
+++ Car.java
@@ -0,0 +1,14 @@
+public class Car{
+    private int id;
+    private String brand;
+    private String model;
+    private double rentPerDay;
+
+    public Car(int id, String brand, String model, double rentPerDay){
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.rentPerDay = rentPerDay;
+    }
+
+}
