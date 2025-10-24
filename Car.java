43e148989bb4b39465bb22a8cc037eef325f6216
TusharJain86 <tusharjain0806@gmail.com>
2025-10-25 00:40:32 +0530

    car class updated

--- Car.java
+++ Car.java
@@ -11,4 +11,23 @@ public class Car{
         this.rentPerDay = rentPerDay;
     }
 
+    //getter and setter methods
+    public int getID(){return id;}
+    public void setID(int id){this.id = id;}
+
+    public String getBrand(){return brand;}
+    public void setBrand(String brand){this.brand = brand;}
+
+    public String getModel(){return model;}
+    public void setModel(String model){this.model = model;}
+
+    public double getRentPerDay(){return rentPerDay;}
+    public void setRentPerDay(double rentPerDay){this.rentPerDay = rentPerDay;}
+
+    //toString method
+    @Override
+    public String toString(){
+        return "Car ID: " + id + ", Brand: " + brand + ", Model: " + model + ", Rent per Day: $" + rentPerDay;
+    }
+
 }
